6463944da6a0a74768199c5c5e88bded4eeed8ef
Saket Shubham <saketshubham.bt21civil@pec.edu.in>
2022-07-11 09:42:19 +0530

    Update Sak8_assign6.py

--- Sak8_assign6.py
+++ Sak8_assign6.py
@@ -77,29 +77,23 @@ print("\b ")
 
 
 #6
+class student_data:
+    def _init_(self, name, branch):
+        self.name = name
+        self.branch = branch
 
-def student_data(student_name="N.A.",student_class="N.A"):
-    global student_id
-    print("### Data Print ###")
-    print("Student ID : ",student_id)
-    if(student_name!="N.A."):
-        print("Student Name : ",student_name)
-    if(student_class!="N.A."):
-        print("Student Class : ",student_class)
-student_id=input("Enter the Student ID : ")
-cond=input("If you want to enter only student name then press n, only student class then c or both then b : ")
-if(cond=="n"):
-    s_name=input("Enter the Student's Name : ")
-    student_data(s_name)
-elif(cond=="c"):
-    s_class=input("Enter the Student's class : ")
-    student_data(s_class)
-elif(cond=="b"):
-    s_name=input("Enter the Student's Name : ")
-    s_class=input("Enter the Student's class : ")
-    student_data(s_name,s_class)
-else:
-    student_data()
+    def student_id(self):
+        print(f'name : {self.name} ||| branch : {self.branch}')
+
+
+s = student_data('aniket', 'civil')
+# function
+s.student_id()
+
+# arguments
+print('--->', s.name)
+print('--->', s.branch)
+
 
 
 #7
